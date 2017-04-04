9ed5ab87eb1097248957b472acaee570d757eefe
Lyzzeth Parody Álvarez <lizparody@gmail.com>
2017-04-04 10:05:37 -0500

    learning object construction

---
+++ car.js
@@ -0,0 +1,125 @@
+/* function Car(make, model, year, color, passengers, convertible, mileage) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.passengers = passengers;
+  this.convertible = convertible;
+  this.mileage = mileage;
+  this.started = false;
+
+  this.start = function() {
+    this.started = true;
+  };
+
+  this.stop = function() {
+    this.started = false;
+  };
+
+  this.drive = function() {
+    if (this.started) {
+      console.log(this.make + " " + this.model + " goes zoom zoom");
+    } else {
+      console.log("start the engine first")
+    }
+  };
+}
+
+var chevy = new Car("Chevy", "Bel Air", 1957, "red", 2, false, 1021);
+var cadi = new Car("GM", "Cadillac", 1955, "tan", 5, false, 12892);
+var taxi = new Car("Webville Motors", "Taxi", 1955, "yellow", 4, false, 281341);
+var fiat = new Car("Fiat", "500", 1957, "Medium Blue", 2, false, 88000);
+
+var testCar = new Car("Webville Motors", "Test Car", 2014, "marine", 2, true, 21);
+
+var cars = [chevy, cadi, taxi, fiat, testCar];
+
+for (var i = 0; i < cars.length; i++){
+  cars[i].start();
+  cars[i].drive();
+  cars[i].drive();
+  cars[i].stop();
+} */
+
+//There is a lot of parameters for that reason it's better to use object literal.
+
+function Car(params) {
+  this.make = params.make;
+  this.model = params.model;
+  this.year = params.year;
+  this.color = params.color;
+  this.passengers = params.passengers;
+  this.convertible = params.convertible;
+  this.mileage = params.mileage;
+  this.started = false;
+
+  this.start = function() {
+    this.started = true;
+  };
+
+  this.stop = function() {
+    this.started = false;
+  };
+
+  this.drive = function() {
+    if (this.started) {
+      console.log(this.make + " " + this.model + " goes zoom zoom");
+    } else {
+      console.log("start the engine first")
+    }
+  };
+}
+
+var chevyParams = {
+  make: "GM",
+  model: "Cadillac",
+  year: 1955,
+  color: "tan",
+  passengers: 2,
+  convertible: false,
+  mileage:  1021
+}
+
+var cadiParams = {
+  make: "Chevy",
+  model: "Bel Air",
+  year: 1957,
+  color: "red",
+  passengers: 5,
+  convertible: false,
+  mileage:  12892
+};
+
+var taxiParams = {
+  make: "Webville Motors",
+  model: "Taxi",
+  year: 1955,
+  color: "yellow",
+  passengers: 4,
+  convertible: false,
+  mileage: 281341
+}
+
+var fiatParams = {
+  make: "Fiat",
+  model:  "500",
+  year: 1957,
+  color: "Medium Blue",
+  passengers: 2,
+  convertible: false,
+  mileage: 88000
+}
+
+var chevy = new Car(chevyParams);
+var cadi = new Car(cadiParams);
+var taxi = new Car(taxiParams);
+var fiat = new Car(fiatParams);
+
+var cars = [chevy, cadi, taxi, fiat];
+
+for (var i = 0; i < cars.length; i++){
+  cars[i].start();
+  cars[i].drive();
+  cars[i].drive();
+  cars[i].stop();
+}
